7d8b90e96d5a9698c91edbf383d14a29aeb3f81e
cdubowdeveloper <cdubowdeveloper@gmail.com>
2022-10-22 18:21:44 -0500

    Firebase endpoints

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { createEvent, joinEvent, suggestSong, voteSong } from './firebase';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => {
+          createEvent("spotify_playlist_id", "spotify_token").then((res) => {
+            console.log(res);
+          })}
+          }
+        >Create</button>
+        <button onClick={() => {
+          joinEvent("7112", "6094805359").then((res) => {
+            console.log(res);
+          })}
+          }
+        >Join</button>
+        <button onClick={() => {
+          suggestSong("track_id","Fireball", "Pitbull", "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTM7pIhi5YkkNsLK9YPJNn18KQWsotoHyGgZRYkvJNAHHDXgFxQ").then((res) => {
+            console.log(res);
+          })}
+          }
+        >Suggest Fireball by Pitbull</button>
+
+        <button onClick={() => {
+          voteSong("track_id", 1).then((res) => {
+            console.log(res);
+          })}
+          }
+        >Upvote Fireball by Pitbull</button>
+
       </header>
     </div>
   );
