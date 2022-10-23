ccf578b7d70de6d7ae7731267153655998c9f91d
Jianger <ethantianshujiang@gmail.com>
2022-10-22 23:41:46 -0500

    additional styling changes

--- src/components/UserHome/UserHome.js
+++ src/components/UserHome/UserHome.js
@@ -1,7 +1,9 @@
 import IconTextButton from "../../elements/IconTextButton/IconTextButton";
 import playIcon from "../../parts/play.png";
 import musicIcon from "../../parts/music.png";
+import searchIcon from "../../parts/search.png";
 import React from "react";
+import './UserHome.scss';
 
 class UserHome extends React.Component {
     handleSearch(event) {
@@ -11,14 +13,18 @@ class UserHome extends React.Component {
 
     render() {
         return (
-            <div className="container">
-                <h1>User Home</h1>
-                <form onSubmit={this.handleSearch}>
-                    <input className="search-songs" onChange={null/* TODO*/} placeholder="Suggest a song" />
-                </form>
-
-                <IconTextButton icon={playIcon} text="See queue" link="/queue" />
-                <IconTextButton icon={musicIcon} text="Vote on songs" link="/vote" />
+            <div className="container wrapper">
+                <div className="search">
+                    <h1 className="title">Search</h1>
+                    <form onSubmit={this.handleSearch}>
+                        <img className="search-icon" src={searchIcon} alt="search icon"/>
+                        <input className="search-songs" onChange={null/* TODO*/} placeholder="Suggest a song" autoComplete="off" />
+                    </form>
+                </div>
+                <div className='other-actions'>
+                    <IconTextButton className='queue' icon={playIcon} text="See queue" link="/queue" />
+                    <IconTextButton className='vote' icon={musicIcon} text="Vote on songs" link="/vote" />
+                </div>
             </div>
         );
     }
