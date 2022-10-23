797dad77dcdcfcfb567e1fd20d3347f1c7e9e973
alexbawa <alexbawa@gmail.com>
2022-10-22 19:45:54 -0500

    Add initial styles for homepage

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,18 +1,50 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import './Homepage.scss';
 
 class Homepage extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            selected: "join"
+        }
+
+        this.updateSelection = this.updateSelection.bind(this);
+    }
+
+    updateSelection(event) {
+        event.preventDefault();
+        if(event.target.id != this.state.selected) {
+            this.setState({
+                selected: event.target.id
+            });
+        }
+    }
+
+    renderSelection() {
+        let joinClasses = "selection left";
+        let createClasses = "selection right";
+
+        if(this.state.selected == "join") {
+            joinClasses += " selected";
+        } else {
+            createClasses += " selected";
+        }
+        return (
+            <div className="selection-container">
+                <div onClick={this.updateSelection} id="join" className={joinClasses}><p>Join</p></div>
+                <div onClick={this.updateSelection} id="create" className={createClasses}><p>Create</p></div>
+            </div>
+        );
+    }
     render() {
         return (
-            <div class="homepage">
-                <h1>Homepage</h1>
-                <button>
-                    <Link to="/createEvent">Create Event</Link>
-                </button>
-                <button>
-                    <Link to="/joinEvent">Join Event</Link>
-                </button>
-            </div>
+           <div className="homepage">
+                <img className="homepage-logo" src=""></img>
+                <h4 className="homepage-subtitle">Welcome to Waveform!</h4>
+                <p className="homepage-blurb">Join or create an event to get started with your dynamic music selection. Your perfect party playlist awaits.</p>
+                {this.renderSelection()}
+           </div>
         );
     }
 }
