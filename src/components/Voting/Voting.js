8574b0f2d91c80888730d30ff8784eed337e8ce9
Jianger <ethantianshujiang@gmail.com>
2022-10-22 22:53:18 -0500

    finished styling on Queue, Voting and corresponding elements

--- src/components/Voting/Voting.js
+++ src/components/Voting/Voting.js
@@ -2,14 +2,31 @@ import { Component } from 'react';
 import BackButton from '../../elements/BackButton/BackButton.js';
 import SongWithVoting from '../../elements/SongWithVoting/SongWithVoting.js';
 import testAlbumArt from '../../parts/testAlbum.jpg';
+import './Voting.scss';
 
 class Voting extends Component {
     render() {
         return (
             <div className='container'>
-                <BackButton link='/userHome'/>
-                <h1>Voting</h1>
-                <SongWithVoting albumart={testAlbumArt} name='Flashing Lights Flashing Lights Flashing Lightsssssssssss' artist='Kanye West'/>
+                <div className='header'>
+                    <BackButton className="back" link='/userHome' />
+                    <h1 className="title">Voting</h1>
+                </div>
+                <div className='songs'>
+                    <SongWithVoting albumart={testAlbumArt} name='Flashing Lights Flashing Lights Flashing Lightsssssssssss' artist='Kanye West' />
+                    <SongWithVoting albumart={testAlbumArt} name='Flashing Lights Flashing Lights Flashing Lightsssssssssss' artist='Kanye West' />
+                    <SongWithVoting albumart={testAlbumArt} name='Flashing Lights Flashing Lights Flashing Lightsssssssssss' artist='Kanye West' />
+                    <SongWithVoting albumart={testAlbumArt} name='Flashing Lights Flashing Lights Flashing Lightsssssssssss' artist='Kanye West' />
+                    <SongWithVoting albumart={testAlbumArt} name='Flashing Lights Flashing Lights Flashing Lightsssssssssss' artist='Kanye West' />
+                    <SongWithVoting albumart={testAlbumArt} name='Flashing Lights Flashing Lights Flashing Lightsssssssssss' artist='Kanye West' />
+                    <SongWithVoting albumart={testAlbumArt} name='Flashing Lights Flashing Lights Flashing Lightsssssssssss' artist='Kanye West' />
+                    <SongWithVoting albumart={testAlbumArt} name='Flashing Lights Flashing Lights Flashing Lightsssssssssss' artist='Kanye West' />
+                    <SongWithVoting albumart={testAlbumArt} name='Flashing Lights Flashing Lights Flashing Lightsssssssssss' artist='Kanye West' />
+                    <SongWithVoting albumart={testAlbumArt} name='Flashing Lights Flashing Lights Flashing Lightsssssssssss' artist='Kanye West' />
+                    <SongWithVoting albumart={testAlbumArt} name='Flashing Lights Flashing Lights Flashing Lightsssssssssss' artist='Kanye West' />
+                    <SongWithVoting albumart={testAlbumArt} name='Flashing Lights Flashing Lights Flashing Lightsssssssssss' artist='Kanye West' />
+                    <SongWithVoting albumart={testAlbumArt} name='Flashing Lights Flashing Lights Flashing Lightsssssssssss' artist='Kanye West' />
+                </div>
             </div>
         );
     }
