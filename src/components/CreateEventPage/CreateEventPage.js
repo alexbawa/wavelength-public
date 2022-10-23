55b4107e250da1d2a62ab005723da65a1cf38799
alexbawa <alexbawa@gmail.com>
2022-10-22 20:06:17 -0500

    Setup spotify login flow

--- src/components/CreateEventPage/CreateEventPage.js
+++ src/components/CreateEventPage/CreateEventPage.js
@@ -1,16 +1,22 @@
 import React from "react";
 import SpotifyInterface from "../../util/SpotifyInterface";
+import './CreateEventPage.scss';
+
 
 class CreateEventPage extends React.Component {
     async componentDidMount() {
-        let token = await SpotifyInterface.getToken();
-        this.props.setToken(token);
+        setTimeout(async () => {
+            let token = await SpotifyInterface.getToken();
+            this.props.setToken(token);
+        }, 2000)
     }
 
     render() {
         return (
             <div className="create-event-page">
-                <h1>Create Event Page</h1>
+                <h2 className="create-event-title">Login to Spotify</h2>
+                <p className="create-event-subtitle">Time to authenticate - you will be redirected shortly.</p>
+                <img className="create-event-spotify-logo" src=""></img>
             </div>
         );
     }
