5938458b1ea058b7ab87d8f1efcb067e65caffa3
alexbawa <alexbawa@gmail.com>
2022-10-22 23:54:37 -0500

    start search integration

--- src/components/UserHome/UserHome.js
+++ src/components/UserHome/UserHome.js
@@ -4,11 +4,42 @@ import musicIcon from "../../parts/music.png";
 import searchIcon from "../../parts/search.png";
 import React from "react";
 import './UserHome.scss';
+import SpotifyInterface from "../../util/SpotifyInterface";
+import Song from "../../elements/Song/Song";
 
 class UserHome extends React.Component {
-    handleSearch(event) {
-        event.preventDefault();
-        console.log("searching...");
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchTerm: null,
+            searchResults: [],
+        }
+
+        this.updateSearch = this.updateSearch.bind(this);
+        this.search = this.search.bind(this);
+        this.renderSearchResults = this.renderSearchResults.bind(this);
+    }
+    
+    async search() {
+        if(this.state.searchTerm) {
+            let results = await SpotifyInterface.search(this.state.searchTerm, );
+            this.setState({
+                searchResults: results
+            });
+        }
+    }
+
+    updateSearch(event) {
+        this.setState({
+            searchTerm: event.target.value,
+        })
+        this.search();
+    }
+
+    renderSearchResults() {
+        return this.state.searchResults.map(song => {
+            return <Song name={song.name} artist={song.artist} albumart={song.coverart}/>
+        })
     }
 
     render() {
@@ -16,10 +47,13 @@ class UserHome extends React.Component {
             <div className="container wrapper">
                 <div className="search">
                     <h1 className="title">Search</h1>
-                    <form onSubmit={this.handleSearch}>
+                    <form >
                         <img className="search-icon" src={searchIcon} alt="search icon"/>
-                        <input className="search-songs" onChange={null/* TODO*/} placeholder="Suggest a song" autoComplete="off" />
+                        <input className="search-songs" onChange={this.updateSearch} placeholder="Suggest a song" autoComplete="off" />
                     </form>
+                    <div className="search-results">
+                        {this.renderSearchResults()}
+                    </div>
                 </div>
                 <div className='other-actions'>
                     <IconTextButton className='queue' icon={playIcon} text="See queue" link="/queue" />
